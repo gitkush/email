30790c0d77eb6c010379d966e268d3e4b76e8b80
Ankush Gaur <38980954+gitkush@users.noreply.github.com>
2021-06-03 15:43:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,99 +1,93 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import pydeck as pdk
-import plotly.express as px
+import re
+import smtplib
+import socket
+import dns.resolver
 
 
 
-DATA_URL = "Data.csv"
 
 
-st.title("Motor Vehicle Collisions n New York")
+def main():
 
-st.markdown("This application is a streamlit dashboard ")
+	
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL,nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
-    data.dropna(subset=['LATITUDE','LONGITUDE'], inplace=True)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data.rename(columns={'crash_date_crash_time':'date/time'}, inplace = True)
-    return data
+	st.title("Verify Email Address")
+	st.sidebar.title("Enter email here:")
 
 
-data = load_data(100000)
-original_data = data
+	email = st.sidebar.text_input('Email to verify:')
 
 
-st.header("Where are the most people injured in NYC")
-injured_people = st.slider("Number of persons injured in vehice collisions:",0,19)
-st.map(data.query("injured_persons >= @injured_people")[["latitude","longitude"]].dropna(how="any"))
+    # GSHEET_LINK = st.sidebar.text_input('Link to "readable" GSHEET with input:')
+
+	if st.sidebar.button('Verify'):
+		if(email==""):
+			st.write("Please input email")
+		else:
+			verify_email(email)
 
 
-st.header("How many collisions occured during a given time of day")
-hour = st.slider("Hour to look at:", 0,23)
-data = data[data["date/time"].dt.hour == hour]
+def verify_email(email):
 
-st.markdown("Vehicle collisions between %i:00 and %i:00" %(hour, (hour+1) %24))
+	try:
 
-midpoint = (np.average(data['latitude']), np.average(data['longitude']))
+		email_address = email
 
-st.write(pdk.Deck(
-        map_style="mapbox://styles/mapbox/light-v9",
-        initial_view_state = {
-            "latitude" : midpoint[0],
-            "longitude" : midpoint[1],
-            "zoom":11,
-            "pitch":50
-        },
-        layers = [
-            pdk.Layer(
-                "HexagonLayer",
-                data = data[['date/time','latitude', 'longitude']],
-                get_position = ['longitude', 'latitude'],
-                radius = 100,
-                extruded = True,
-                pickable = True,
-                elevation_scale = 4,
-                elevation_range = [0,1000]
-            )
-        ]
-))
+		#Step 1: Check email
+		#Check using Regex that an email meets minimum requirements, throw an error if not
+		addressToVerify = email
+		match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', addressToVerify)
 
+		if match == None:
+		    st.write('Bad Syntax in ' + addressToVerify)
+		    raise ValueError('Bad Syntax')
 
-st.subheader("Breakdown by minute between %i:00 and %i:00" %(hour, (hour + 1) %24))
-filtered = data[
-    (data['date/time'].dt.hour >= hour) & (data["date/time"].dt.hour < (hour + 1))
-]
+		#Step 2: Getting MX record
+		#Pull domain name from email address
+		domain_name = email_address.split('@')[1]
 
-# st.write(filtered)
+		#get the MX record for the domain
+		records = dns.resolver.query(domain_name, 'MX')
+		mxRecord = records[0].exchange
+		mxRecord = str(mxRecord)
 
-hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0,60))[0]
-chart_data = pd.DataFrame({'minute': range(60), 'crashes':hist})
+		#Step 3: ping email server
+		#check if the email address exists
 
-# st.write(chart_data)
+		# Get local server hostname
+		host = socket.gethostname()
 
-fig = px.bar(chart_data, x='minute', y='crashes', hover_data = ['minute', 'crashes'], height = 400)
-st.write(fig)
+		# SMTP lib setup (use debug level for full output)
+		server = smtplib.SMTP()
+		server.set_debuglevel(0)
 
+		# SMTP Conversation
+		server.connect(mxRecord)
+		server.helo(host)
+		server.mail('me@domain.com')
+		code, message = server.rcpt(str(addressToVerify))
+		server.quit()
 
-st.header("Top 5 dangerous street by affected type")
-select = st.selectbox("Affected type of people",["Pedestrians","Cyclists","Motorists"])
-
-if select == "Pedestrians":
-    st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending = False).dropna(how='any')[:5])
-
-elif select == "Cyclists":
-    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending = False).dropna(how='any')[:5])
-    
-else:
-    st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=['injured_motorists'], ascending = False).dropna(how='any')[:5])
+		# Assume 250 as Success
+		if code == 250:
+		    st.write('Valid email 🟢')
+		else:
+		    print('Invalid email 🔴')
 
 
-if st.checkbox("Show raw data", False):
-    st.subheader('Raw Data')
-    st.write(data)
+	except:
+		st.write("Uncaught error. Improvements required")
+
+	return
 
 
+def write_data(dframe):
+	st.write(dframe)
+
+
+
+
+if __name__ == '__main__':
+	main()
+ 
